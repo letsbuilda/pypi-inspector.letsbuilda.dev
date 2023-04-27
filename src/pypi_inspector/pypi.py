d3445cecff5d5f1cb3bf6e66fbaef3b343cbf7d3
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-04-26 21:02:46 -0500

    display the commit hash
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/pypi_inspector/pypi.py
+++ src/pypi_inspector/pypi.py
@@ -1,5 +1,7 @@
 """main"""
 
+from os import getenv
+
 from flask import Blueprint
 
 pypi_bp = Blueprint("pypi", __name__, template_folder="templates")
@@ -8,4 +10,6 @@ pypi_bp = Blueprint("pypi", __name__, template_folder="templates")
 @pypi_bp.route("/")
 async def index():
     """index"""
-    return "<h1>Hello, World!</h1>"
+    release_prefix = getenv("PYPI_INSPECTOR_SENTRY_RELEASE_PREFIX", "pypi_inspector")
+    git_sha = getenv("GIT_SHA", "development")
+    return f"<h1>{release_prefix}@{git_sha}</h1>"
